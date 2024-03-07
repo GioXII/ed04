05ed2bc25a90326b4c355afcb20eb2271e74fd8f
Sergio <gioixii@gmail.com>
2024-03-07 11:11:22 +0100

    Comentarios añadidos a las clases Contacto.java y Agenda.java

--- src/main/java/org/example/Agenda.java
+++ src/main/java/org/example/Agenda.java
@@ -1,4 +1,10 @@
 package org.example;
+/**
+ * Esta clase sirve para fijar las acciones sobre los contactos de la agenda.
+ * @author Sergio Paco Gómez
+ * @since 2024
+ * @version 1.0
+ */
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -7,10 +13,19 @@ import java.util.List;
 public class Agenda {
     private List<Contacto> contacts; // Lista de Contacto
 
+    /**
+     * Método para crear la agenda con los contactos.
+     */
     public Agenda() {
         this.contacts = new ArrayList<>();
     }
 
+    /**
+     *
+     * @param name
+     * @param phone
+     * Método para añadir un {@link Contacto} a la agenda.
+     */
     public void addContact(String name, String phone) {
         boolean exists = false;
         for (Contacto c : contacts) {
@@ -27,6 +42,11 @@ public class Agenda {
         }
     }
 
+    /**
+     *
+     * @param name
+     * Método para borrar un {@link Contacto} de la agenda.
+     */
     public void removeContact(String name) {
         Iterator<Contacto> it = contacts.iterator();
 
@@ -39,6 +59,13 @@ public class Agenda {
         }
     }
 
+    /**
+     *
+     * @param name
+     * @param oldPhone
+     * @param newPhone
+     * Método para modificar el número de teléfono de un {@link Contacto}.
+     */
     public void modifyPhoneNumber(String name, String oldPhone, String newPhone) {
         for (Contacto c : contacts) {
             if (c.getName().equalsIgnoreCase(name)) {
@@ -53,6 +80,11 @@ public class Agenda {
         }
     }
 
+    /**
+     *
+     * @return
+     * Devuelve los {@link Contacto}s
+     */
     public List<Contacto> getContacts() {
         return this.contacts;
     }
--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -1,4 +1,10 @@
 package org.example;
+/**
+ * Esta clase sirve para fijar los atributos de los contactos de la agenda.
+ * @author Sergio Paco Gómez
+ * @since 2024
+ * @version 1.0
+ */
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,16 +13,30 @@ class Contacto {
     private String name;
     private List<String> phones;
 
+    /**
+     *
+     * @param name
+     * @param phone
+     * Crea un Contacto con los datos introducidos.
+     */
     public Contacto(String name, String phone) {
         this.name = name;
         this.phones = new ArrayList<>();
         this.phones.add(phone);
     }
 
+    /**
+     * Devuelve el nombre del {@link Contacto}.
+     * @return
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Devuelve el teléfono del {@link Contacto}.
+     * @return
+     */
     public List<String> getPhones() {
         return this.phones;
     }
